437353260148e24c27055ae75c8ecc116639faeb
messanger <messanger>
2023-03-31 17:22:47 +0300

    start debaging registration form

--- WindowsFormsClient/RegistrationForm.Designer.cs
+++ WindowsFormsClient/RegistrationForm.Designer.cs
@@ -53,9 +53,10 @@ namespace WindowsFormsClient
             this.enter_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.enter_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.enter_button.Font = new System.Drawing.Font("Gagalin", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.enter_button.Location = new System.Drawing.Point(130, 464);
+            this.enter_button.Location = new System.Drawing.Point(116, 371);
+            this.enter_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.enter_button.Name = "enter_button";
-            this.enter_button.Size = new System.Drawing.Size(228, 56);
+            this.enter_button.Size = new System.Drawing.Size(203, 45);
             this.enter_button.TabIndex = 13;
             this.enter_button.Text = "ПРИНЯТЬ";
             this.enter_button.UseVisualStyleBackColor = true;
@@ -68,9 +69,10 @@ namespace WindowsFormsClient
             this.password_button.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.password_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.password_button.Image = ((System.Drawing.Image)(resources.GetObject("password_button.Image")));
-            this.password_button.Location = new System.Drawing.Point(410, 402);
+            this.password_button.Location = new System.Drawing.Point(364, 322);
+            this.password_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.password_button.Name = "password_button";
-            this.password_button.Size = new System.Drawing.Size(40, 33);
+            this.password_button.Size = new System.Drawing.Size(36, 26);
             this.password_button.TabIndex = 12;
             this.password_button.UseVisualStyleBackColor = false;
             this.password_button.Click += new System.EventHandler(this.password_button_Click);
@@ -79,9 +81,9 @@ namespace WindowsFormsClient
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(181, 373);
+            this.label2.Location = new System.Drawing.Point(161, 298);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 29);
+            this.label2.Size = new System.Drawing.Size(104, 25);
             this.label2.TabIndex = 11;
             this.label2.Text = "password";
             // 
@@ -89,35 +91,41 @@ namespace WindowsFormsClient
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(212, 303);
+            this.label1.Location = new System.Drawing.Point(188, 242);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 29);
+            this.label1.Size = new System.Drawing.Size(58, 25);
             this.label1.TabIndex = 10;
             this.label1.Text = "login";
             // 
             // password_textBox
             // 
-            this.password_textBox.Location = new System.Drawing.Point(81, 405);
+            this.password_textBox.Location = new System.Drawing.Point(72, 324);
+            this.password_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.password_textBox.Name = "password_textBox";
-            this.password_textBox.Size = new System.Drawing.Size(323, 26);
+            this.password_textBox.Size = new System.Drawing.Size(288, 22);
             this.password_textBox.TabIndex = 9;
             this.password_textBox.UseSystemPasswordChar = true;
+            this.password_textBox.Enter += new System.EventHandler(this.password_textBox_Enter);
+            this.password_textBox.Leave += new System.EventHandler(this.password_textBox_Leave);
             // 
             // login_textBox
             // 
-            this.login_textBox.Location = new System.Drawing.Point(81, 335);
+            this.login_textBox.Location = new System.Drawing.Point(72, 268);
+            this.login_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.login_textBox.Name = "login_textBox";
-            this.login_textBox.Size = new System.Drawing.Size(323, 26);
+            this.login_textBox.Size = new System.Drawing.Size(288, 22);
             this.login_textBox.TabIndex = 8;
+            this.login_textBox.Enter += new System.EventHandler(this.login_textBox_Enter);
+            this.login_textBox.Leave += new System.EventHandler(this.login_textBox_Leave);
             // 
             // reg_label
             // 
             this.reg_label.AutoSize = true;
             this.reg_label.Font = new System.Drawing.Font("GOST type A", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.reg_label.ForeColor = System.Drawing.Color.Lime;
-            this.reg_label.Location = new System.Drawing.Point(115, 60);
+            this.reg_label.Location = new System.Drawing.Point(102, 48);
             this.reg_label.Name = "reg_label";
-            this.reg_label.Size = new System.Drawing.Size(262, 51);
+            this.reg_label.Size = new System.Drawing.Size(225, 42);
             this.reg_label.TabIndex = 7;
             this.reg_label.Text = "РЕГИСТРАЦИЯ";
             this.reg_label.MouseDown += new System.Windows.Forms.MouseEventHandler(this.reg_label_MouseDown);
@@ -127,9 +135,9 @@ namespace WindowsFormsClient
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(181, 211);
+            this.label3.Location = new System.Drawing.Point(161, 169);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(130, 29);
+            this.label3.Size = new System.Drawing.Size(110, 25);
             this.label3.TabIndex = 17;
             this.label3.Text = "Фамилия";
             // 
@@ -137,17 +145,18 @@ namespace WindowsFormsClient
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(212, 141);
+            this.label4.Location = new System.Drawing.Point(188, 113);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(66, 29);
+            this.label4.Size = new System.Drawing.Size(57, 25);
             this.label4.TabIndex = 16;
             this.label4.Text = "Имя";
             // 
             // userSecondName_textBox
             // 
-            this.userSecondName_textBox.Location = new System.Drawing.Point(81, 243);
+            this.userSecondName_textBox.Location = new System.Drawing.Point(72, 194);
+            this.userSecondName_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.userSecondName_textBox.Name = "userSecondName_textBox";
-            this.userSecondName_textBox.Size = new System.Drawing.Size(323, 26);
+            this.userSecondName_textBox.Size = new System.Drawing.Size(288, 22);
             this.userSecondName_textBox.TabIndex = 15;
             this.userSecondName_textBox.Enter += new System.EventHandler(this.userSecondName_textBox_Enter);
             this.userSecondName_textBox.Leave += new System.EventHandler(this.userSecondName_textBox_Leave);
@@ -155,9 +164,10 @@ namespace WindowsFormsClient
             // userFirstName_textBox
             // 
             this.userFirstName_textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.userFirstName_textBox.Location = new System.Drawing.Point(81, 173);
+            this.userFirstName_textBox.Location = new System.Drawing.Point(72, 138);
+            this.userFirstName_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.userFirstName_textBox.Name = "userFirstName_textBox";
-            this.userFirstName_textBox.Size = new System.Drawing.Size(323, 26);
+            this.userFirstName_textBox.Size = new System.Drawing.Size(287, 22);
             this.userFirstName_textBox.TabIndex = 14;
             this.userFirstName_textBox.Enter += new System.EventHandler(this.userFirstName_textBox_Enter);
             this.userFirstName_textBox.Leave += new System.EventHandler(this.userFirstName_textBox_Leave);
@@ -170,9 +180,10 @@ namespace WindowsFormsClient
             this.closing_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MintCream;
             this.closing_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MintCream;
             this.closing_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.closing_button.Location = new System.Drawing.Point(438, 12);
+            this.closing_button.Location = new System.Drawing.Point(389, 10);
+            this.closing_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.closing_button.Name = "closing_button";
-            this.closing_button.Size = new System.Drawing.Size(40, 33);
+            this.closing_button.Size = new System.Drawing.Size(36, 26);
             this.closing_button.TabIndex = 18;
             this.closing_button.Text = "X";
             this.closing_button.UseVisualStyleBackColor = true;
@@ -183,9 +194,9 @@ namespace WindowsFormsClient
             // register_link
             // 
             this.register_link.AutoSize = true;
-            this.register_link.Location = new System.Drawing.Point(146, 548);
+            this.register_link.Location = new System.Drawing.Point(130, 438);
             this.register_link.Name = "register_link";
-            this.register_link.Size = new System.Drawing.Size(194, 20);
+            this.register_link.Size = new System.Drawing.Size(171, 17);
             this.register_link.TabIndex = 19;
             this.register_link.TabStop = true;
             this.register_link.Text = "У меня уже есть аккаунт";
@@ -194,11 +205,11 @@ namespace WindowsFormsClient
             // RegistrationForm
             // 
             this.AcceptButton = this.enter_button;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MintCream;
             this.CancelButton = this.closing_button;
-            this.ClientSize = new System.Drawing.Size(490, 602);
+            this.ClientSize = new System.Drawing.Size(436, 482);
             this.Controls.Add(this.register_link);
             this.Controls.Add(this.closing_button);
             this.Controls.Add(this.label3);
@@ -214,6 +225,7 @@ namespace WindowsFormsClient
             this.Controls.Add(this.reg_label);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "RegistrationForm";
