fe2bdce90cc2feb2b769ae5ea67b207a985c0613
messanger <messanger>
2023-04-03 15:52:33 +0300

    added to server db class for conections with data base

---
+++ ASPCoreServer/DB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Data.OleDb;
+using System.Data.SqlClient;
+//using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore;
+
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPCoreServer
+{
+    public class DB
+    {
+    }
+}
